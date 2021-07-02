2fa4199af250242f3e45e38e38b0cc7ddc9905e7
hv90 <hugo.machado@hi.com.br>
2021-07-02 11:22:10 -0300

    start RCC to RFC transcode

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import React, {useState} from 'react';
-import {SafeAreaView, View, Button, NativeModules} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Button,
+  NativeModules,
+  findNodeHandle,
+  UIManager,
+  Platform,
+  Dimensions,
+} from 'react-native';
 
 import BitmovinPlayer from './src/RNBitmovinPlayer';
+import RNBitmovinPlayer from './src/RNFCBitmovinPlayer';
 
 const App = () => {
   const [isFullscreen, setIsFullscreen] = useState(false);
   const BitmovinPlayerModule = NativeModules.RNBitmovinPlayer;
-  console.log(NativeModules);
+  console.log(
+    'screen: ',
+    Dimensions.get('screen'),
+    'window: ',
+    Dimensions.get('window'),
+  );
   const config = {
     source: {url: 'https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd'},
   };
+
   return (
-    <SafeAreaView style={{flex: 0.5, width: '100%'}}>
-      <View style={{flex: 1}}>
-        <BitmovinPlayer
-          style={{flex: 1, height: '100%'}}
+    <SafeAreaView
+      style={{
+        flex: 0.5,
+        width: '100%',
+      }}>
+      <View style={{flex: 1, height: '100%', maxHeight: 600}}>
+        <RNBitmovinPlayer
+          style={{flex: 1, height: 500, maxHeight: 600}}
           configuration={config}
         />
       </View>
